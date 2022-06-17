917ccb9916fdefe0dc36338e404a9c4a9c46162a
Murtaja Ahmed <mdmurtaja1996@gmail.com>
2022-06-17 09:29:47 +0600

    1st upload

---
+++ resources/js/main.js
@@ -0,0 +1,17 @@
+function openNav() { document.getElementById("myNav").style.width = "100%" } function closeNav() { document.getElementById("myNav").style.width = "0%" } $(document).ready(function () { $(".js--services-section").waypoint(function (a) { "down" == a ? $("nav").addClass("sticky") : $("nav").removeClass("sticky") }), $("nav ul li a").click(function () { $("nav ul li a").removeClass("active"), $(this).addClass("active") }), $("nav a img.logo").click(function () { $("nav ul li a").removeClass("active"), $("nav ul li:first-child a").addClass("active") }); mixitup(".container"); $("a").on("click", function (a) { if ("" !== this.hash) { a.preventDefault(); var t = this.hash; $("html, body").animate({ scrollTop: $(t).offset().top }, 800, function () { window.location.hash = t }) } }) }); $("svg.radial-progress").each(function (e, t) { $(this).find($("circle.complete")).removeAttr("style") }), $(window).scroll(function () { $("svg.radial-progress").each(function (e, t) { $(window).scrollTop() > $(this).offset().top - .75 * $(window).height() && $(window).scrollTop() < $(this).offset().top + $(this).height() - .25 * $(window).height() && (percent = $(t).data("percentage"), radius = $(this).find($("circle.complete")).attr("r"), circumference = 2 * Math.PI * radius, strokeDashOffset = circumference - percent * circumference / 100, $(this).find($("circle.complete")).animate({ "stroke-dashoffset": strokeDashOffset }, 1250)) }) }).trigger("scroll");
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
